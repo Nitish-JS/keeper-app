31c07f700d935f34e0d23c2b305a784cba41fd4a
Nitish-JS <jsnitish46@gmail.com>
2022-02-16 23:19:37 +0530

    keeper app

---
+++ build/precache-manifest.35061539b8a9307c06c4031cdb721976.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfe80ef3946da82d50ba75a01f6dfff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "b861a95bcf3bf21e5e1b",
+    "url": "/static/js/main.be06a67c.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
